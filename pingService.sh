13ee5d5ba4042e026bad11c02d31e1a8576d9a3b
Jacopo1891 <jacopo1891@gmail.com>
2022-12-10 00:39:22 +0100

    Update pingService.sh

--- pingService.sh
+++ pingService.sh
@@ -15,7 +15,7 @@ messageOnline="Server ${serverName} (${server}:${port}) now up!%0A$(date +"%a, %
 messageOffline="Server ${serverName} (${server}:${port}) down!%0A$(date +"%a, %d %B %Y, %H:%M:%S")"
 
 function sendTelegramMessage() {
-        echo $( curl --write-out %{http_code} --silent --output /dev/null -X POST -s --data "chat_id=${chatId}" --data "disable_web_page_preview=true"  --data "text=$1" --connect-timeout 30 --max-time 45  "https://api.telegram.org/bo    t${botToken}/sendMessage" )
+        echo $( curl --write-out %{http_code} --silent --output /dev/null -X POST -s --data "chat_id=${chatId}" --data "disable_web_page_preview=true"  --data "text=$1" --connect-timeout 30 --max-time 45  "https://api.telegram.org/bot${botToken}/sendMessage" )
     }
 if ! nc -w 2 -z $server $port > /dev/null 2>&1
 then
